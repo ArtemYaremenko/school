b5e8a50a6b228ef13f50d15d6ed868ff0d7c3fc0
Artem <artem.yaremenko.1992@gmail.com>
2025-03-11 03:12:18 +0300

    InfoController

---
+++ src/main/java/ru/hogwarts/school/service/Impl/InfoServiceImpl.java
@@ -0,0 +1,19 @@
+package ru.hogwarts.school.service.Impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.service.InfoService;
+@Service
+public class InfoServiceImpl implements InfoService {
+
+    Logger logger = LoggerFactory.getLogger(InfoServiceImpl.class);
+    @Value("${server.port}")
+    private Integer port;
+    @Override
+    public Integer getPort() {
+        logger.debug("getPort - {}", port);
+        return port;
+    }
+}
